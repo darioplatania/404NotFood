f399c613b375a77d6419241bf588078259d3e468
dario platania <dariopl91@gmail.com>
2017-03-22 20:53:44 +0100

    insert welcome message(first_step)

--- fez_spider/fez_spider/Program.cs
+++ fez_spider/fez_spider/Program.cs
@@ -12,16 +12,38 @@ using Microsoft.SPOT.Touch;
 using Gadgeteer.Networking;
 using GT = Gadgeteer;
 using GTM = Gadgeteer.Modules;
+using GHI.Glide;
+using GHI.Glide.Display;
+using GHI.Glide.UI;
 
 namespace fez_spider
 {
     public partial class Program
     {
+        private static GHI.Glide.Display.Window window;
+        private static TextBlock textBlock;
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
             // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
             Debug.Print("Program Started");
+
+            /*welcome into display*/
+            first_step();
+        }
+
+        static void first_step()
+        {
+            window = GlideLoader.LoadWindow(Resources.GetString(Resources.StringResources.Window));
+
+            GlideTouch.Initialize();
+
+            textBlock = (TextBlock)window.GetChildByName("textBlock");
+            //textBlock.TapEvent += OnTap;
+
+            Glide.MainWindow = window;
+
+            
         }
     }
 }

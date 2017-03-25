f20aa3dec5be097a79790ade63bb7695a34412bd
dario platania <dariopl91@gmail.com>
2017-03-25 14:59:35 +0100

    Create Menu' Grid

--- fez_spider/fez_spider/Program.cs
+++ fez_spider/fez_spider/Program.cs
@@ -20,12 +20,10 @@ namespace fez_spider
 {
     public partial class Program
     {
-        private static GHI.Glide.Display.Window window;       
-        private static TextBlock textBlock;
-        private static Button button;      
+        private static GHI.Glide.Display.Window window;             
+        private static GHI.Glide.Display.Window menu; 
+              
 
-        private static GHI.Glide.Display.Window menu;
-        private static TextBlock menu_istance;
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
@@ -33,50 +31,41 @@ namespace fez_spider
             Debug.Print("Program Started");
 
             /*welcome into display*/
-            first_step();        
+            first_step();          
 
-        }
+        }        
+
 
         /****************
          * FUNCTION 
          * *************/
-        static void first_step()
+            static void first_step()
         {
             window = GlideLoader.LoadWindow(Resources.GetString(Resources.StringResources.Window));
 
             GlideTouch.Initialize();
-
-            /*print welcome message into textblock*/
-            textBlock = (TextBlock)window.GetChildByName("textBlock");
+            Glide.MainWindow = window;    
 
             /*create button to start*/
-            button = (Button)window.GetChildByName("button");
+            var button = (Button)window.GetChildByName("button");
             /*press button event*/            
             button.PressEvent += Button_PressEvent;
 
-            Glide.MainWindow = window;
+            
         }
-
 
         /****************
          * CALLBACK 
          * *************/
         private static void Button_PressEvent(object sender)
         {
-            try
-            {
-                Debug.Print("Premuto");                    
+           
+                Debug.Print("Premuto..");                    
                 /*load menu*/
-                //menu = GlideLoader.LoadWindow(Resources.GetString(Resources.StringResources.Menu));
-                /*print menu*/
-                //menu_istance = (TextBlock)window.GetChildByName("menu_istance");
-                
-             }
-            catch (Exception e)
-            {
-               Debug.Print(e.Message);
-            }
-
+                menu = GlideLoader.LoadWindow(Resources.GetString(Resources.StringResources.Menu));
+                Glide.MainWindow = menu;           
+            
+                       
         }
                 
     }

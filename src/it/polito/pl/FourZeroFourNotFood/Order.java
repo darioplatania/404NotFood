407cd056224524321cd04ff0ad6516c913dab165
Carmelo Riolo <carmeloriolo93@gmail.com>
2017-04-29 17:09:34 +0200

    Added Order + Food classes

---
+++ src/it/polito/pl/FourZeroFourNotFood/Order.java
@@ -0,0 +1,47 @@
+package it.polito.pl.FourZeroFourNotFood;
+
+import java.util.ArrayList;
+
+public class Order {
+
+	
+	private String name;
+	private String amount;
+	private ArrayList<Food> foods;
+	
+	public Order(String name, String amount, ArrayList<Food> foods) {
+		this.name = name;
+		this.amount = amount;
+		this.foods = foods;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public ArrayList<Food> getFoods() {
+		return foods;
+	}
+
+	public void setFoods(ArrayList<Food> foods) {
+		this.foods = foods;
+	}
+	
+	
+	
+	
+	
+	
+}

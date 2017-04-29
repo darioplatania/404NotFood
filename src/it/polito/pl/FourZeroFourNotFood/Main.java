35528305b683122de3d2667be08b670176d9b989
Carmelo Riolo <carmeloriolo93@gmail.com>
2017-04-29 17:22:14 +0200

    added Logger to Main Class

--- src/it/polito/pl/FourZeroFourNotFood/Main.java
+++ src/it/polito/pl/FourZeroFourNotFood/Main.java
@@ -1,14 +1,39 @@
 package it.polito.pl.FourZeroFourNotFood;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
+	
+	// SET APP NAME
+	private static final String APP_NAME = "404 Not Food";
+	
+	// SETUP LOGGER
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+	
+	
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		
+		
+		// System Starting...
+		DEBUG_INFO(Level.INFO,APP_NAME+" Starting...");
+		
+		// TODO: 1. Start Connection on IPv4 Socket
+		
+		
+		
+		// TODO: 2. Wait for oders
+		// TODO: 2.1 Follow order workflow
+		// TODO: 2.2 Close Order
 		
 		
 		
 		
-		
+	}
+	
+	private static void DEBUG_INFO(Level lvl, String msg){
+		LOGGER.log(lvl, msg);
 	}
 	
 }
